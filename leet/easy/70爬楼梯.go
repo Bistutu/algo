303a9f856a9c5c9c79f36118db4954712b522454
ThinkStu <1914493943@qq.com>
2023-09-22 09:05:08 +0800

    1

--- leet/easy/70爬楼梯.go
+++ leet/easy/70爬楼梯.go
@@ -7,7 +7,13 @@ import (
 
 func main() {
 	now := time.Now()
-	println(climbStairs2(3))
+	println(climbStairs1(30))
+	fmt.Println(time.Now().Sub(now).String())
+	now = time.Now()
+	println(climbStairs2(30))
+	fmt.Println(time.Now().Sub(now).String())
+	now = time.Now()
+	println(climbStairs3(30))
 	fmt.Println(time.Now().Sub(now).String())
 }
 
@@ -29,4 +35,25 @@ func climbStairs2(n int) int {
 		pre, cur = cur, pre+cur
 	}
 	return cur
+}
+
+// 记忆化存储
+func climbStairs3(n int) int {
+	mem := make([]int, n+1)
+	for k := range mem {
+		mem[k] = -1 // -1 表示暂无记录
+	}
+	return handler(n, mem)
+}
+
+func handler(n int, mem []int) int {
+	if n <= 2 {
+		return n
+	}
+	if mem[n] != -1 {
+		return mem[n]
+	}
+	count := handler(n-1, mem) + handler(n-2, mem)
+	mem[n] = count
+	return count
 }
