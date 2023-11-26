17421010a7061e97e4f8763ebd699df1addd15e8
miankang.chen <91378285+Bistutu@users.noreply.github.com>
2023-11-26 11:04:21 +0800

    1

--- leet/hard/42接雨水.go
+++ leet/hard/42接雨水.go
@@ -8,6 +8,27 @@ func main() {
 }
 
 func trap(height []int) int {
+	// 双指针
+	left, right := 0, len(height)-1
+	count := 0
+	maxLeft, maxRight := 0, 0
 
-	return 0
+	for left < right {
+		if height[left] < height[right] {
+			if maxLeft > height[left] {
+				count += maxLeft - height[left]
+			} else {
+				maxLeft = height[left]
+			}
+			left++
+		} else {
+			if maxRight > height[right] {
+				count += maxRight - height[right]
+			} else {
+				maxRight = height[right]
+			}
+			right--
+		}
+	}
+	return count
 }
